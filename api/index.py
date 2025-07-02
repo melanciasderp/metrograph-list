492b16d181a403fdcb41c970814b71aa21139852
Melancias Machado <melanciasogrande@proton.me>
2025-07-02 13:21:51 +0100

    Added Date to Query for more accuracy

--- api/index.py
+++ api/index.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify
-import requests, os
+import requests, os, re
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 TMDB_API_KEY =  os.getenv("TMDB_API_KEY") # Replace with your actual TMDb API key
+pattern=r"\b\d{4}\b" # match release date from website
 
-def get_tmdb_id(title):
+def get_tmdb_id(title, date=""):
     search_url = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&query={title}"
+    if date != "":
+        search_url+=f"&year={date}"
     response = requests.get(search_url)
     if response.status_code == 200:
         results = response.json().get('results', [])
@@ -17,24 +20,36 @@ def get_tmdb_id(title):
     return {}
 
 @app.route('/')
-def get_movies():
+def get_movie_details():
     # Scrape movie titles from Metrograph
     url = "https://metrograph.com/film/"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    movie_titles = [title.text.strip() for title in soup.select('h3')]
+    movie_banners = soup.find_all("div",{"class":"col-sm-12"})
 
-    # Create a list of dictionaries with movie titles and TMDb IDs
     movies = []
-    for title in movie_titles:
-        tmdb = get_tmdb_id(title)
-        tmdb_id = tmdb.get('id',None)
-        movie = {
-            'title': title,
-            'TmdbId': tmdb_id,
-            'id': tmdb_id
-        }
-        movies.append(movie) if tmdb_id else None
+
+    for movie_details in movie_banners:
+        try:
+            movie_name = movie_details.select("h3")[0].text.strip()
+            details = [a.text.strip() for a in movie_details.select("h5")]
+            date=""
+
+            for text in details:
+                date_match = re.search(pattern,text)
+                if date_match:
+                    date = date_match.group(0)
+
+            tmdb = get_tmdb_id(movie_name,date)
+            tmdb_id = tmdb.get('id',None)
+            movie = {
+                'title': movie_name,
+                'TmdbId': tmdb_id,
+                'id': tmdb_id,
+            }
+            movies.append(movie) if tmdb_id else None
+        except IndexError:
+            pass
 
     return jsonify(movies)
 
